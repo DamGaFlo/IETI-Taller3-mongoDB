ff25386d3b3067baec982c21dafa6b258ea5c205
unknown <johan.garrido@labinfo.is.escuelaing.edu.co>
2022-01-28 15:11:59 -0500

    proyecto generado

---
+++ src/main/java/co/edu/escuelaing/ieti/lvl2api/Lvl2apiApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.ieti.lvl2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lvl2apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lvl2apiApplication.class, args);
+	}
+
+}
